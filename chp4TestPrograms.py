bf4b4bba6514cb6daf598d054dd52bea35fe8746
dmwellsSDBD <55493178+dmwellsSDBD@users.noreply.github.com>
2023-01-02 19:21:44 -0500

    Initial commit

---
+++ chp4TestPrograms.py
@@ -0,0 +1,39 @@
+'''
+#1.    
+Write a script named numberlines.py. This script creates a program listing from a source program. This script should 
+prompt the user for the names of two files that exist inside of your current github repository for this test. The input 
+filename could be the name of the script itself, but be careful to use a different output filename! The script copies 
+the lines of text from the input file to the output file, numbering each line as it goes. The line numbers should be 
+right-justified in 4 columns, so that the format of a line in the output file looks like this example:
+
+1>  This is the first line of text
+2>  This is the second line of text
+etc.
+etc.
+etc.  
+'''
+
+
+'''
+#2.
+Octal numbers have a base of eight and the digits 0–7. Write the script to convert numbers between the octal and decimal 
+representations of integers. These scripts use algorithms that are similar to those of the binaryToDecimal script developed 
+in Section 4-3.
+'''
+
+
+'''
+#3.
+Write a file that will read all the lines from anotherFile.txt and print the out to the terminal line by line.
+'''
+
+
+'''
+#4.
+Write a script named dif.py. This script should prompt the user for the names of two text files and compare the 
+contents of the two files to see if they are the same (PLEASE use example4a.txt and example4b.txt as your inputs). 
+If they are, the script should simply output "Yes". If they are not, the script should output "No", followed by 
+the first lines of each file that differ from each other. The input loop should read and compare lines from each 
+file. The loop should break as soon as a pair of different lines is found.
+'''
+
